ad57d60a032fb2ed662639770d59a2eb34f2c0f9
KangKang <ws2005ncsd@outlook.com>
2022-07-31 15:16:41 +0800

    ok

---
+++ InjectHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FastWin32.Diagnostics;
+
+namespace SimpleInjectorLib
+{
+    public class InjectHelper
+    {
+        //Injector
+        public uint ProcessId { get; set; }
+        public string AssemblyPath { get; set; }
+        public string TypeName { get; set; }
+        public string MethodName { get; set; }
+        public string? Argument { get; set; }
+
+        public bool TryInject()
+        {
+            if (AssemblyPath != null && TypeName != null && MethodName != null)
+            {
+                if (Argument != null)
+                {
+                    return Injector.InjectManaged(ProcessId, AssemblyPath, TypeName, MethodName, Argument);
+                }
+                else
+                {
+                    return Injector.InjectManaged(ProcessId, AssemblyPath, TypeName, MethodName, "");
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ProcessHelper.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace SimpleInjectorLib
+{
+    public class ProcessHelper
+    {
+        public List<Process> ProcessList { get; private set; }
+
+        public ProcessHelper()
+        {
+            ProcessList = new List<Process>();
+            var processArray = Process.GetProcesses();
+            foreach (var process in processArray)
+                ProcessList.Add(process);
+        }
+
+        public void Refresh()
+        {
+            _refresh();
+        }
+
+        public Process? GetProcessByName(string name)
+        {
+            _refresh();
+            foreach (var process in ProcessList)
+            {
+                if (process.ProcessName == name)
+                {
+                    return process;
+                }
+            }
+            return null;
+        }
+
+        public Process? GetProcessById(int id)
+        {
+            _refresh();
+            foreach (var process in ProcessList)
+            {
+                if (process.Id == id)
+                {
+                    return process;
+                }
+            }
+            return null;
+        }
+
+        public Process? GetProcessByTitle(string title)
+        {
+            _refresh();
+            foreach (var process in ProcessList)
+            {
+                if (process.MainWindowTitle == title)
+                {
+                    return process;
+                }
+            }
+            return null;
+        }
+
+        private void _refresh()
+        {
+            var processArray = Process.GetProcesses();
+            ProcessList.Clear();
+            foreach (var process in processArray)
+                ProcessList.Add(process);
+        }
+    }
+}
